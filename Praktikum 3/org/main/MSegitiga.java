d54e9c9d86d14e7a2593e4d5531b6a5310f5cbb5
MhamanK <100416105+MhamanK@users.noreply.github.com>
2023-03-08 17:18:04 +0700

    Delete MSegitiga.java

--- Praktikum 3/org/main/MSegitiga.java
+++
@@ -1,9 +0,0 @@
-package org.main;
-
-import org.bangundatar.*;
-
-public class MSegitiga{
-	public static void main(String[] args) {
-		Segitiga segitiga = new Segitiga(10,10,3);
-		segitiga.printInfo();
-		System.out.println("Luas Segitiga : "+segitiga.hitungLuas());
